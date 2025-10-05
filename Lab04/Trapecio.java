8d1012924c3f1aab753d425b7b218ffc75f6d6fd
Jh4stYn <jpayehuancar@unsa.edu.pe>
2025-10-04 19:04:14 -0500

    feat: implementación del método principal de integración paralela

--- Lab04/Trapecio.java
+++ Lab04/Trapecio.java
@@ -32,5 +32,27 @@ class TrabajadorTrapecio extends Thread {
 }
 
 public class Trapecio {
+    // Método que calcula la integral con el método del trapecio paralelo
+    public static double integrar(FuncionUnivariable f, double a, double b, int n, int numHilos)
+            throws InterruptedException {
+        double h = (b - a) / n;
+        TrabajadorTrapecio[] hilos = new TrabajadorTrapecio[numHilos];
+        int tamañoBloque = n / numHilos;
+
+        for (int t = 0; t < numHilos; t++) {
+            int inicio = t * tamañoBloque + 1; // evitar f(a)
+            int fin = (t == numHilos - 1) ? n - 1 : (inicio + tamañoBloque - 1); // evitar f(b)
+            hilos[t] = new TrabajadorTrapecio(f, a, h, inicio, fin);
+            hilos[t].start();
+        }
+
+        double suma = f.evaluar(a) + f.evaluar(b);
+        for (TrabajadorTrapecio hilo : hilos) {
+            hilo.join();
+            suma += 2 * hilo.getSumaParcial();
+        }
+
+        return (h / 2) * suma;
+    }
     public static void main(String[] args)  {}
 }
